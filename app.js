cb11e37e9b6b1532169e91872ade8e2b2c34eb1e
lionelbergen@live.com <lionelbergen@live.com>
2018-11-17 11:24:39 -0600

    reddit test

--- app.js
+++ app.js
@@ -3,13 +3,25 @@ var app = express();
 
 const snoowrap = require('snoowrap');
 
+const requestor = new snoowrap({
+  userAgent: 'Searching for certain keywords in all comments, displaying comments elsewhere',
+  clientId: 'EkRCRBmwlW9c1Q',
+  clientSecret: '_eUrRZr0qQ2ltrCuCg4JoRdj3zA',
+  username: 'Lottery-Bot',
+  password: 'redditFreinds123'
+});
+
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
 
-app.get('/', function (req, res) {
+app.get('/', function (req, res) 
+{
   res.send('Hello World!');
+  console.log(requestor);
+  
+  requestor.getSubreddit('all').getComments();
 });
 
 app.listen(port, function () {
