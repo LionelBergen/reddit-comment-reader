1d221c2ae3da40278e3c68fe5e1b2488f002d523
lionelbergen@live.com <lionelbergen@live.com>
2018-11-17 10:19:33 -0600

    listen on proper port

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var app = express();
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!');
 });
