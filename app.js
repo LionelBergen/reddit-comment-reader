d5e74ad4e838114e83f4a34b0f84e96e901814b3
lionelbergen@live.com <lionelbergen@live.com>
2018-11-18 10:29:03 -0600

    hggg

--- app.js
+++ app.js
@@ -19,22 +19,15 @@ function filterCondition(comment)
 	return comment.body.includes('city');
 }*/
 
-var ioc = require( 'socket.io-client' );
-var client = ioc.connect( "wss://reddit-agree-with-you.herokuapp.com/", {
-	reconnection: true
+let ioc = require( 'socket.io-client' );
+
+var socket = io.connect('https://reddit-agree-with-you.herokuapp.com/', {reconnect: true});
+
+console.log('2');
+
+// Add a connect listener
+socket.on('connect', function(socket) { 
+  console.log('Connected!');
 });
 
-client.on('connect', function () {
-    console.log('connected to localhost:3000');
-    socket.on('clientEvent', function (data) {
-        console.log('message from the server:', data);
-        socket.emit('serverEvent', "thanks server! for sending '" + data + "'");
-    });
-});
-
-/*var HOST = location.origin.replace(/^http/, 'ws')
-      var ws = new WebSocket(HOST);
-      var el = document.getElementById('server-time');
-      ws.onmessage = function (event) {
-        el.innerHTML = 'Server time: ' + event.data;
-      };*/
+console.log('3');
