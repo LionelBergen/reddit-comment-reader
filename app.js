1e46753b57a2ed530cfde5116bc4af3cebdcf099
lionelbergen@live.com <lionelbergen@live.com>
2018-11-18 17:28:57 -0600

    dont allow duplicates

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const intervalToWaitInMillisecondsBetweenReadingComments = 1100;
 let faye = require('faye');
 let client = new faye.Client('http://reddit-agree-with-you.herokuapp.com/');
 
+let commentCache = getArrayWithLimitedLength(1200, false);
+
 setInterval(function() {
 	requestor.getNewComments('all').filter(filterCondition).forEach(comment => processComment(comment));
 }, intervalToWaitInMillisecondsBetweenReadingComments);
@@ -22,11 +24,32 @@ function filterCondition(comment)
 	var myregExp = new RegExp("^/r/theydidthemath$", 'i');
 	var regexp2 = new RegExp("^(no you|no u|nou)$", 'i');
 	
-	return myregExp.test(comment.body) || regexp2.test(comment.body);
-	//
+	return (myregExp.test(comment.body) || regexp2.test(comment.body))
+		&& !commentCache.includes(comment);
 }
 
 function processComment(comment)
 {
+	commentCache.push(comment);
+	
 	client.publish('/messages', {comment: comment.body});
+}
+
+function getArrayWithLimitedLength(length, allowDuplicates) 
+{
+	var array = new Array();
+
+	array.push = function () {
+		if (!allowDuplicates && this.includes(arguments[0]))
+		{
+			return null;
+		}
+		if (this.length >= length) {
+			this.shift();
+		}
+		return Array.prototype.push.apply(this,arguments);
+	}
+
+	return array;
+
 }
