eb3864f2f0a41d259451de78f1c8922468efd42d
lionelbergen@live.com <lionelbergen@live.com>
2018-11-18 13:20:09 -0600

    cleanup

--- app.js
+++ app.js
@@ -10,6 +10,17 @@ const requestor = new Snoowrap({
 
 const intervalToWaitInMillisecondsBetweenReadingComments = 1100;
 
+let faye = require('faye');
+let client = new faye.Client('http://reddit-agree-with-you.herokuapp.com/');
+
+/*var x = */client.publish('/messages', {text: 'hfklgjflkjg'});
+/*
+x.then(function() {
+  console.log('Message received by server!');
+}, function(error) {
+  console.log('There was a problem: ' + error.message);
+});*/
+
 /*setInterval(function() {
 	requestor.getNewComments('all').filter(filterCondition).forEach(comment => console.log(comment.body));
 }, intervalToWaitInMillisecondsBetweenReadingComments);
@@ -18,17 +29,3 @@ function filterCondition(comment)
 {
 	return comment.body.includes('city');
 }*/
-
-let io = require( 'socket.io-client' );
-let faye = require('faye');
-
-let client = new faye.Client('http://reddit-agree-with-you.herokuapp.com/');
-
-client.publish("/messages", {
-		text: 'gjfhklghjdfklhgfdkjhg'
-});
-
-client.subscribe('/messages', function(message) {
-	console.log('ggggggggggggggggggggggggggggggggggggggggglkfjgljkfdkjgfdjkgfdjklgjdf');
-  // etc
-});
