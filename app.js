1f76cc696221a08cb774813a132f7d217a223ab6
lionelbergen@live.com <lionelbergen@live.com>
2018-11-18 13:21:50 -0600

    read comments and publish

--- app.js
+++ app.js
@@ -13,19 +13,11 @@ const intervalToWaitInMillisecondsBetweenReadingComments = 1100;
 let faye = require('faye');
 let client = new faye.Client('http://reddit-agree-with-you.herokuapp.com/');
 
-/*var x = */client.publish('/messages', {text: 'hfklgjflkjg'});
-/*
-x.then(function() {
-  console.log('Message received by server!');
-}, function(error) {
-  console.log('There was a problem: ' + error.message);
-});*/
-
-/*setInterval(function() {
-	requestor.getNewComments('all').filter(filterCondition).forEach(comment => console.log(comment.body));
+setInterval(function() {
+	requestor.getNewComments('all').filter(filterCondition).forEach(comment => client.publish('/messages', {text: comment.body}));
 }, intervalToWaitInMillisecondsBetweenReadingComments);
 
 function filterCondition(comment)
 {
 	return comment.body.includes('city');
-}*/
+}
