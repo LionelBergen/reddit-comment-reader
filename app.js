ffb98551cc2a33aec548bcb5565e944f470d6fd9
lionelbergen@live.com <lionelbergen@live.com>
2018-11-18 01:13:18 -0600

    added logging

--- app.js
+++ app.js
@@ -21,4 +21,17 @@ function filterCondition(comment)
 
 const net = require('net');
 
-const client = net.createConnection({ port: 16801, host: '34.227.214.181' });
+const client = net.createConnection({ port: 16801, host: '34.227.214.181' }, () => {
+  // 'connect' listener
+  console.log('connected to server!');
+  client.write('world!\r\n');
+});
+
+client.on('data', (data) => {
+  console.log(data.toString());
+  client.end();
+});
+
+client.on('end', () => {
+  console.log('disconnected from server');
+});
