d612b647305e09991f0ece2ba49f629e8dc87e05
lionelbergen@live.com <lionelbergen@live.com>
2018-11-26 12:27:32 -0600

    pg connect

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ let pg = require('pg');
 // load all env variables from .env file into process.env object.
 require('dotenv').config();
 
-let intervalToWaitInMillisecondsBetweenReadingComments = 1200;
+let intervalToWaitInMillisecondsBetweenReadingComments = 5200;
 let intervalToWaitBeforeSendingIdleMessage = 10;
 
 var lastMessageSentAt = new Date().getTime();
