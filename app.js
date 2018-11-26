beb76f651e39e50976b8240810deafd6fd7a523a
lionelbergen@live.com <lionelbergen@live.com>
2018-11-26 16:49:30 -0600

    removed dotenv from npm

--- app.js
+++ app.js
@@ -15,9 +15,6 @@ let requestor = new Snoowrap({
 
 let pg = require('pg');
 
-// load all env variables from .env file into process.env object.
-//require('dotenv').config();
-
 let intervalToWaitInMillisecondsBetweenReadingComments = 1200;
 let intervalToWaitBeforeSendingIdleMessage = 30;
 let commentCacheSize = 2400;
