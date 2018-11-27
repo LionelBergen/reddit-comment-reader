f040278f5a0c9bb89eb5199f6806870f4d502682
lionelbergen@live.com <lionelbergen@live.com>
2018-11-26 20:10:56 -0600

    Creating our own reddit api

--- RedditClient.js
+++ RedditClient.js
@@ -39,7 +39,7 @@ function getRawResponseFromUrl(url)
 
 	// The whole response has been received. Print out the result.
 	resp.on('end', () => {
-		console.log(JSON.parse(data).explanation);
+		console.log(data);   //JSON.parse(data).explanation);
 	});
 
 	}).on("error", (err) => {
