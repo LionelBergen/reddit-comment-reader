052d60ee220e2d079967144577ba9730b2e113c8
dustytrash <Lionelbergen@live.com>
2021-01-03 17:52:41 -0600

    cleanup

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 // Local files
-require('./reddit_comment_reader/CommonTools.js')();
 require('./reddit_comment_reader/DiscordSender.js')();
 const DatabaseUtil = require('./reddit_comment_reader/tools/DatabaseUtil.js');
 const ErrorHandler = require('./reddit_comment_reader/tools/ErrorHandler.js');
@@ -20,9 +19,6 @@ const dissallowedSubreddits = ['suicidewatch', 'depression' ];
 
 let lastMessageSentAt = new Date().getTime();
 
-// TODO: remove these comments. Just keeping it here for reference
-// const clientConnection = isLocal() ? 'http://localhost:8000/' : 'http://reddit-agree-with-you.herokuapp.com/';
-
 if (!process.env.DATABASE_URL) {
   throw 'Please set process.env.DATABASE_URL! e.g SET DATABASE_URL=postgres://.....';
 } else if (!process.env.DISCORD_TOKEN) {
@@ -31,21 +27,15 @@ if (!process.env.DATABASE_URL) {
   throw 'Please set Reddit client URL.';
 }
 
+// Important: The clientTagName's, are referenced from the Database. (public.RegexpComment.Handle)
 const agreeWithYouClient = new MessagingClients.FayeMessagingClient({clientTagName:'Agree-with-you', blacklistedSubreddits:dissallowedSubreddits, receivingMessagesURL:process.env.AGREE_WITH_YOU_URL, 
     timeBetweenSamePostInSubreddit:secondsTimeToWaitBetweenPostingSameCommentToASubreddit});
 const discordClient = new MessagingClients.DiscordMessagingClient({clientTagName:'DISCORD', blacklistedSubreddits:[], discordToken:process.env.DISCORD_TOKEN, 
     timeBetweenSamePostInSubreddit:secondsTimeToWaitBetweenPostingSameCommentToASubredditForDiscord});
-
-// Important: The clientTagName's, are referenced from the Database. (public.RegexpComment.Handle)
 ClientHandler.addClients(
   agreeWithYouClient,
   discordClient
 );
-
-/*
-console.log('is local?: ' + isLocal());
-console.log('connecting to: ' + clientConnection);
-console.log('Database URL: ' + process.env.DATABASE_URL);*/
 
 // Read data from database, then start the application 
 DatabaseUtil.getCommentSearchObjectsFromDatabase(process.env.DATABASE_URL).then(start).catch(console.error);
