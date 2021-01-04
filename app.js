e81efc48b882bf60d149413bd3b66bf1b2ff6638
dustytrash <Lionelbergen@live.com>
2021-01-03 19:38:06 -0600

    added 2nd discord bot

--- app.js
+++ app.js
@@ -18,8 +18,10 @@ const dissallowedSubreddits = ['suicidewatch', 'depression' ];
 
 if (!process.env.DATABASE_URL) {
   throw 'Please set process.env.DATABASE_URL! e.g SET DATABASE_URL=postgres://.....';
-} else if (!process.env.DISCORD_TOKEN) {
-  throw 'please set process.env.DISCORD_TOKEN!';
+} else if (!process.env.DISCORD_TOKEN_PERSONAL) {
+  throw 'please set process.env.DISCORD_TOKEN_PERSONAL!';
+} else if (!process.env.DISCORD_TOKEN_DENTON) {
+  throw 'Please set DISCORD_TOKEN_DENTON.';
 } else if (!process.env.AGREE_WITH_YOU_URL) {
   throw 'Please set Reddit client URL.';
 }
@@ -29,11 +31,14 @@ const errorHandler = new ErrorHandler(process.env.DATABASE_URL);
 // Important: The clientTagName's, are referenced from the Database. (public.RegexpComment.Handle)
 const agreeWithYouClient = new MessagingClients.FayeMessagingClient({clientTagName:'Agree-with-you', blacklistedSubreddits:dissallowedSubreddits, receivingMessagesURL:process.env.AGREE_WITH_YOU_URL, 
   timeBetweenSamePostInSubreddit:secondsTimeToWaitBetweenPostingSameCommentToASubreddit});
-const discordClient = new MessagingClients.DiscordMessagingClient({clientTagName:'DISCORD', blacklistedSubreddits:[], discordToken:process.env.DISCORD_TOKEN, 
+const discordClientPersonal = new MessagingClients.DiscordMessagingClient({clientTagName:'DISCORD_PERSONAL', blacklistedSubreddits:[], discordToken:process.env.DISCORD_TOKEN_PERSONAL, 
+  timeBetweenSamePostInSubreddit:secondsTimeToWaitBetweenPostingSameCommentToASubredditForDiscord});
+const discordClientDenton = new MessagingClients.DiscordMessagingClient({clientTagName:'DISCORD', blacklistedSubreddits:[], discordToken:process.env.DISCORD_TOKEN_DENTON, 
   timeBetweenSamePostInSubreddit:secondsTimeToWaitBetweenPostingSameCommentToASubredditForDiscord});
 ClientHandler.addClients(
   agreeWithYouClient,
-  discordClient
+  discordClient,
+  discordClientDenton
 );
 
 // Read data from database, then start the application 
