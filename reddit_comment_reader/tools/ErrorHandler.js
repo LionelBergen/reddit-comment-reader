788532a9b9e27dd11fa639e279a8c078f1860cfa
dustytrash <Lionelbergen@live.com>
2021-01-12 14:39:20 -0600

    change to Error Handler.js

--- reddit_comment_reader/tools/ErrorHandler.js
+++ reddit_comment_reader/tools/ErrorHandler.js
@@ -1,5 +1,6 @@
 const DatabaseUtil = require('./DatabaseUtil.js');
 const RedditCommentError = require('./RedditCommentError.js');
+const NetworkDebugger = require('./NetworkDebug.js');
 
 class ErrorHandler {
   constructor(databaseConnectionUrl) {
@@ -9,11 +10,13 @@ class ErrorHandler {
   handleError(error) {
     // Write network status to DB error table
     
-    
     if (typeof error == RedditCommentError) {
       DatabaseUtil.writeErrorToDatabase(this.databaseConnectionUrl, error.error, error.error.stack, error.error.toString(), error.redditComment);
     } else {
-      DatabaseUtil.writeErrorToDatabase(this.databaseConnectionUrl, error, error.stack, error.toString(), undefined);
+      // get network connectivity status
+      NetworkDebugger.getHostPingStatus().then(function(connectionStatus) {
+        DatabaseUtil.writeErrorToDatabase(this.databaseConnectionUrl, error, error.stack, connectionStatus.toString(), undefined);
+      });
     }
   }
 }
