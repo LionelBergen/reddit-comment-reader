398a2c94eb1212f5c0523f019871fd09fe0ebf02
dustytrash <Lionelbergen@live.com>
2024-09-29 15:41:04 -0500

    commonutil test

--- test/common-tools-test.js
+++ test/common-tools-test.js
@@ -2,15 +2,21 @@ import Util from '../reddit-comment-reader/tools/common-tools.js';
 import assert from 'assert';
 
 describe('GetUniqueArray', () => {
-  it('should override duplicates', () => {
-    const subredditModsList = Util.getUniqueArray(3000);
+  it('should generate array', () => {
+    const uniqueArray = Util.getUniqueArray(3000);
 
     const var1 = { id: 'oneTwoThree' };
-    var1.modList = [1, 2, 3, 4, 5];
+    var1.someProperty = [1, 2, 3, 4, 5];
 
-    subredditModsList.push(var1);
+    uniqueArray.push(var1);
 
-    assert.ok(subredditModsList.includes({ id: 'oneTwoThree' }));
-    assert.ok(subredditModsList.get({ id: 'oneTwoThree' }));
+    assert.ok(uniqueArray.includes({ id: 'oneTwoThree' }));
+    assert.ok(uniqueArray.includes(var1));
+    assert.ok(uniqueArray.get({ id: 'oneTwoThree' }));
+    assert.ok(uniqueArray.get({ id: 'oneTwoThree' }).someProperty);
+
+    const result = uniqueArray.get({ id: 'oneTwoThree' }).someProperty;
+    assert.equal(result[0], 1);
+    assert.equal(result[4], 5);
   });
 });
